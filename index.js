86fcc50d74c8499c382c00ac94b57be1640d4703
shakil88818 <shakil88818@gmail.com>
2024-03-07 12:59:15 +0600

    register & error handler

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const dbConnect = require("./config/dbConnect");
-require("dotenv").config()
 const app = express();
-const PORT = process.env.PORT || 4000
+const cors=require("cors")
+const dbConnect = require("./config/dbConnect");
+const AuthRouter = require("./routes/AuthRoute");
+const { notFound, errorHandler } = require("./middlewares/ErrorHandler");
+require("dotenv").config()
+
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use("/api/user", AuthRouter)
+
 
 app.use("/",(req,res)=>{
     res.send("i am home")
 })
 
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(PORT, async()=>{
     await dbConnect()
     console.log(`Server is running at http://localhost:${PORT}`)
 })
+
